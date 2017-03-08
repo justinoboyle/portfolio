a9ef75bd51c8939d561ce7e8ff6ef17fea140284
Justin O'Boyle <justin@justinoboyle.com>
2017-03-08 18:19:29 -0500

    oops

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,11 @@ import { container } from 'react-bootstrap'
 export default () =>
   <div>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
-    <link href="/static/style.css" rel="stylesheet" />
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    <link href="/static/style.css" rel="stylesheet" />   
     <div className="container">
       <Header />
       <Content />
     </div>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossOrigin="anonymous"></script>
   </div>
+//<style dangerouslySetInnerHTML={{ __html: stylesheet }} />
